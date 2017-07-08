956b222574918a77eb6b568b48b133ae3d2d3e66
Rails alpha-blog tutorial <thesecondstageshow@gmail.com>
2017-07-08 17:01:54 +0000

    Add edit update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+      before_action :set_user, only: [:edit, :update, :show, :destroy]
     def new 
         @user = User.new 
     end
@@ -14,9 +15,25 @@ class UsersController < ApplicationController
         end
     end
     
+    def edit
+        
+    end
+    
+    def update
+        if @user.update(user_params)
+            flash[:success] = "You account was updated successfully"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
+    
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
     
+    def set_user
+        @user = User.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :articles
   
   get 'signup', to: 'users#new'
-  resource :users, except: [:new, :show, :destroy]
+  resources :users, except: [:new, :show, :destroy]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
