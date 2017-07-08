581f85f6022c1627631e8fdff318a102794a884e
Rails alpha-blog tutorial <thesecondstageshow@gmail.com>
2017-07-08 14:15:14 +0000

    add secure password and add_password_digest_to_user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706035538) do
+ActiveRecord::Schema.define(version: 20170707165323) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170706035538) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
