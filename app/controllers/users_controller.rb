23e1bba121c7ea1e40a8f28dc96813ad1a648c7b
Rails alpha-blog tutorial <thesecondstageshow@gmail.com>
2017-07-10 09:34:20 +0000

    Add user listing and update show articles view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
       before_action :set_user, only: [:edit, :update, :show, :destroy]
+      
+    def index
+        @users = User.all
+    end
+        
     def new 
-        @user = User.new 
+        
     end
     
     def create
